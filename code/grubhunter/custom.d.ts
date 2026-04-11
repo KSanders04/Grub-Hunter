c1cb2ff522fd977046d21358b088d383136fabd7
KSanders04 <kasanders2004@gmail.com>
2026-04-10 21:49:52 -0400

    feat(auth): integrate NextAuth with GitHub provider and add user ID creation

--- code/grubhunter/custom.d.ts
+++ code/grubhunter/custom.d.ts
@@ -1,8 +1,18 @@
+import { DefaultSession } from "next-auth";
+
 declare global {
   var mongoose: {
     conn: typeof import("mongoose") | null;
     promise: Promise<typeof import("mongoose")> | null;
   };
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      fdlst_private_userId?: string;
+    } & DefaultSession["user"];
+  }
 }
 
 export {};
---
+++ code/grubhunter/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,36 @@
+import GithubProvider from "next-auth/providers/github";
+import { NextApiRequest, NextApiResponse } from "next";
+import { createHash } from "crypto";
+import NextAuth from "next-auth";
+
+const createUserId = (value: string): string => {
+  return createHash("sha256").update(value).digest("hex");
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  return await NextAuth(req, res, {
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+    ],
+    callbacks: {
+      async jwt({ token }) {
+        if (token.email && !token.fdlst_private_userId) {
+          token.fdlst_private_userId = createUserId(token.email);
+        }
+        return token;
+      },
+      async session({ session }) {
+        if (session.user?.email && !session.user.fdlst_private_userId) {
+          session.user.fdlst_private_userId = createUserId(session.user.email);
+        }
+        return session;
+      },
+    },
+  });
+}
